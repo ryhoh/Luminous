c79ff2c9b439574b28ab1e7b91ea0bd4fa0220d3
ryhoh <axis38akasira@gmail.com>
2020-05-21 21:53:03 +0900

    [develop] updatable by serial

--- libLEDMatrix.ino
+++ libLEDMatrix.ino
@@ -3,41 +3,63 @@
 
 #define ARDUINO
 
+#define MATRIX_SIZE 12
+#define WAIT 50
+
 Max7219_8x8 *max7219_8x8;
 String5x7Buffer *string5x7Buffer;
 
-const int wait = 50;
+
+String5x7Buffer *buildBuffer() {
+  String buff = Serial.readString();
+  for (size_t i = 0; i < buff.length(); ++i) {
+    if (buff[i] == '\n') {
+      buff[i] = '\0';
+      break;
+    }
+  }
+  
+  return new String5x7Buffer(MATRIX_SIZE, buff.c_str());
+}
 
 void setup(){
-  char text[] = "Hello wooooorld!";
-  string5x7Buffer = new String5x7Buffer(8, text);
-  max7219_8x8 = new Max7219_8x8(8, 11, 10, 13);
+  Serial.begin(9600);
+  
+  max7219_8x8 = new Max7219_8x8(MATRIX_SIZE, 11, 10, 13);
   max7219_8x8->init();
   max7219_8x8->test();
+  
+  string5x7Buffer = new String5x7Buffer(MATRIX_SIZE, "Hello LEDMatrix!");
 }
 
 void loop(){
   while (string5x7Buffer->distToLeftSet() > 0) {
     max7219_8x8->print(string5x7Buffer);
     string5x7Buffer->leftScroll(false);
-    delay(wait);
+    delay(WAIT);
   }
   delay(500);
 
   while (string5x7Buffer->distToRightSet() > 0) {
     max7219_8x8->print(string5x7Buffer);
     string5x7Buffer->leftScroll(false);
-    delay(wait);
+    delay(WAIT);
   }
   delay(500);
 
   while (string5x7Buffer->distToAfter() > 0) {
     max7219_8x8->print(string5x7Buffer);
     string5x7Buffer->leftScroll(false);
-    delay(wait);
+    delay(WAIT);
   }
   delay(500);
   
   string5x7Buffer->reset();
 //  string5x7Buffer->fill(false);
+
+  if (Serial.available()) {
+      delete string5x7Buffer;
+      string5x7Buffer = NULL;
+      string5x7Buffer = buildBuffer();
+  }
 }
